eefd40e6c4a1623b34a49cbebd89dd28ea4a3bc4
Philip Callender <philcal@mac.com>
2012-11-19 02:54:02 +0800

    auto-commit

---
+++ widgets/ttdemo_pages/junk10_13/1537646f451b727bb7680e8bab0abff481ebbea0/junk10_13.js
@@ -0,0 +1,23 @@
+// v3
+
+/**
+ *  Page Javascript - always gets included in this page.
+ */
+
+var thisPage = (function() {
+  // variables
+  var abc = 123;
+
+  // object
+  return {
+    init: function() {
+      //alert("init()");
+    },
+
+    //--------
+    nocomma: null
+  };
+}());
+
+// Init after the page has loaded
+jQuery(thisPage.init);
